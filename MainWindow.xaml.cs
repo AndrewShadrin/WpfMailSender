dae75636d68c0e956b9ca597a4daae0b259fd396
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-04-28 12:57:47 +0300

    Выполнено задание 2 урока. Свой компонент на основе ToolBar не очень получился

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmailSend;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,18 +21,35 @@ namespace WpfMailSender
     /// </summary>
     public partial class MainWindow : Window
     {
-        EmailSendServiceClass emailSendService = new EmailSendServiceClass();
         public MainWindow()
         {
             InitializeComponent();
-            Server.Text = AppConfigClass.ServerName;
+            cbSenderSelect.ItemsSource = VariablesClass.Senders;
+            cbSenderSelect.DisplayMemberPath = "Key";
+            cbSenderSelect.SelectedValuePath = "Value";
+            DBClass db = new DBClass();
+            dgRecievers.ItemsSource = db.Emails;
+            cbServerSelect.ItemsSource = db.Servers;
+            cbServerSelect.DisplayMemberPath = "Name";
+            cbServerSelect.SelectedValuePath = "Port";
+            STB.Items = db.Servers;
+            STB.DisplayMemberPath = "Name";
+            STB.SelectedValuePath = "Port";
+            STB.LblSelectText = "Выбрать сервер";
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                emailSendService.Send(Sender.Text, Reciever.Text, Subject.Text, MessageBody.Document.ToString(), Server.Text, AppConfigClass.ServerPort, Password.Password);
+                using (var emailSendService = new EmailSendService(cbServerSelect.Text, Int32.Parse(cbServerSelect.SelectedValue.ToString()), cbSenderSelect.Text, cbSenderSelect.Text, pbPassword.Password))
+                {
+                    TextRange text = new TextRange(MessageBody.Document.ContentStart, MessageBody.Document.ContentEnd);
+                    foreach (Email email in (IQueryable<Email>)dgRecievers.ItemsSource)
+                    {
+                        emailSendService.Send(email.Value, Subject.Text, text.Text);
+                    } 
+                }
             }
             catch (Exception ex)
             {
@@ -39,5 +57,36 @@ namespace WpfMailSender
                 windowMessage.Show();
             }
         }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            SchedulerClass sc = new SchedulerClass();
+            TimeSpan tsSendTime = sc.GetSendTime(tbTimePicker.Text);
+            if (tsSendTime == new TimeSpan())
+            {
+                MessageBox.Show("Некорректный формат даты");
+                return;
+            }
+            DateTime dtSendDateTime = (cldSchedulDateTimes.SelectedDate ?? DateTime.Today).Add(tsSendTime);
+            if (dtSendDateTime < DateTime.Now)
+            {
+                MessageBox.Show("Дата и время отправки писем не могут быть раньше, чем настоящее время");
+                return;
+            }
+            EmailSendService emailSender = new EmailSendService(cbServerSelect.Text, Int32.Parse(cbServerSelect.SelectedValue.ToString()), cbSenderSelect.Text, cbSenderSelect.Text, pbPassword.Password);
+            sc.SendEmails(dtSendDateTime, emailSender, (IQueryable<Email>)dgRecievers.ItemsSource, Subject.Text, new TextRange(MessageBody.Document.ContentStart, MessageBody.Document.ContentEnd));
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void tscTabSwitcher_btnNextClick(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedIndex = 1;
+        }
+
     }
 }
